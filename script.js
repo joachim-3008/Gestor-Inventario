55e2e63503e56a3a41b893daa76ee0d26cf89308
Joachim Pérez <joachim3008@gmail.com>
2026-03-03 21:35:50 +0800

    update

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let inventario = [];
 
-const nombre = document.querySelector('#nombre-pro');
-const precio = document.querySelector('#precio-pro');
-const stock = document.querySelector('#stock-pro');
-const buton = document.querySelector('#boton');
+
 const contenedorCarta = document.querySelector('#card');
+const aggProducto = document.querySelector('#formulario');
+const padre = document.querySelector('.contenedor');
+
+
 // -------------------------------------------------------------
 const datosAlmacenados = localStorage.getItem('inventario');
 
@@ -13,6 +14,53 @@ const datosAlmacenados = localStorage.getItem('inventario');
 ): null
 
 // -------------------------------------------------------------
+
+aggProducto.addEventListener("click", ()=>{
+
+    padre.innerHTML = `
+        <div class="contenedor-inf">
+            <input type="text" id="nombre-pro" class="inf" maxlength="29" placeholder="Escribe el nombre del producto">
+            <input type="number" id="precio-pro" class="inf" maxlength="29" placeholder="Escribe el precio del producto">
+            <input type="number" id="stock-pro" class="inf" maxlength="29" placeholder="Escribe la cantidad del producto">
+            <button id="boton" class="buttons">Agregar</button>
+        </div>
+        `;
+
+const nombre = document.querySelector('#nombre-pro');
+const precio = document.querySelector('#precio-pro');
+const stock = document.querySelector('#stock-pro');
+const buton = document.querySelector('#boton');
+
+buton.addEventListener('click', () => {
+
+    const nuevoPro = {
+        nombre: nombre.value,
+        precio: parseFloat(precio.value),
+        stock: parseInt(stock.value)
+    };
+
+
+console.log(inventario);
+
+(nuevoPro.nombre && nuevoPro.precio && nuevoPro.stock) ?(
+    inventario.push(nuevoPro),
+    localStorage.setItem('inventario', JSON.stringify(inventario)), //Aqui le decimos al navegador toma esta lista de objetos q cree y almacenala 
+    crearCarta(nuevoPro),
+    limpiarTarjeta()
+) : alert('Rellene todos los campos');
+
+
+}); //este es agg el producto al inv
+
+const limpiarTarjeta = () => {
+    nombre.value = "";
+    precio.value = "";
+    stock.value = "";
+};
+
+}); //este es agg el formulario a la pantalla
+
+
 
 const crearCarta = (nuevoPro) =>{
     const carta = document.createElement('div');
@@ -36,33 +84,4 @@ btnEliminar.addEventListener("click", ()=>{
     contenedorCarta.appendChild(carta);
 };
 
- inventario.forEach(pro => crearCarta(pro))
-
-const limpiarTarjeta = () => {
-    nombre.value = "";
-    precio.value = "";
-    stock.value = "";
-};
-
-
-
-buton.addEventListener('click', () => {
-
-    const nuevoPro = {
-        nombre: nombre.value,
-        precio: parseFloat(precio.value),
-        stock: parseInt(stock.value)
-    };
-
-
-console.log(inventario);
-
-(nuevoPro.nombre && nuevoPro.precio && nuevoPro.stock) ?(
-    inventario.push(nuevoPro),
-    localStorage.setItem('inventario', JSON.stringify(inventario)), //Aqui le decimos al navegador toma esta lista de objetos q cree y almacenala 
-    crearCarta(nuevoPro),
-    limpiarTarjeta()
-) : alert('Rellene todos los campos')
-
-
-});
+ inventario.forEach(pro => crearCarta(pro));
